8a7c4d4d1c46d48a2766eeecafa24d5793ad68a3
Anitha Malaghan <147544944+Anitha-Malaghan@users.noreply.github.com>
2023-12-07 22:36:38 +0530

    ecommerce website with Unit testing using JUnit framework

--- src/main/java/com/example/productservice_proxy/models/Categories.java
+++ src/main/java/com/example/productservice_proxy/models/Categories.java
@@ -20,5 +20,6 @@ public class   Categories extends BaseModel{
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     @Fetch(FetchMode.SUBSELECT)
     private List<Product> productList;
+
 }
 //Mapped is available for one to one, one to many and many to many
